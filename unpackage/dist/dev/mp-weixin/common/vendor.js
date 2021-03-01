11521561ab5da5989604a89b4ac0ade14cbc2007
jin <1076241935@qq.com>
2021-03-01 13:48:41 +0800

    修改接口地址

--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -1932,7 +1932,7 @@ function normalizeComponent (
 "use strict";
 /* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.myUploadFile = exports.myHttp = void 0;var _regenerator = _interopRequireDefault(__webpack_require__(/*! ./node_modules/@babel/runtime/regenerator */ 12));
 var _commom = __webpack_require__(/*! @/util/js/commom.js */ 15);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {try {var info = gen[key](arg);var value = info.value;} catch (error) {reject(error);return;}if (info.done) {resolve(value);} else {Promise.resolve(value).then(_next, _throw);}}function _asyncToGenerator(fn) {return function () {var self = this,args = arguments;return new Promise(function (resolve, reject) {var gen = fn.apply(self, args);function _next(value) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);}function _throw(err) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);}_next(undefined);});};}function ownKeys(object, enumerableOnly) {var keys = Object.keys(object);if (Object.getOwnPropertySymbols) {var symbols = Object.getOwnPropertySymbols(object);if (enumerableOnly) symbols = symbols.filter(function (sym) {return Object.getOwnPropertyDescriptor(object, sym).enumerable;});keys.push.apply(keys, symbols);}return keys;}function _objectSpread(target) {for (var i = 1; i < arguments.length; i++) {var source = arguments[i] != null ? arguments[i] : {};if (i % 2) {ownKeys(Object(source), true).forEach(function (key) {_defineProperty(target, key, source[key]);});} else if (Object.getOwnPropertyDescriptors) {Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));} else {ownKeys(Object(source)).forEach(function (key) {Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));});}}return target;}function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
-var BASE_URL = _commom.socketSeverUrl;
+// const BASE_URL = socketSeverUrl
 // const BASE_URL = 'http://192.168.43.207:3001'
 // 同时发送异步代码的次数，防止一次点击中有多次请求，用于处理
 // let ajaxTimes=0;
@@ -1954,7 +1954,7 @@ var myHttp = function myHttp(options) {
   return new Promise(function (resolve, reject) {
 
     uni.request({
-      url: BASE_URL + options.url,
+      url: _commom.BASE_URL + options.url,
       method: options.method || 'POST',
       data: options.data || {},
       header: header,
@@ -2000,7 +2000,7 @@ var myUploadFile = function myUploadFile(options) {
   return new Promise(function (resolve, reject) {
 
     uni.uploadFile({
-      url: BASE_URL + options.url,
+      url: _commom.BASE_URL + options.url,
       method: options.method || 'POST',
       header: header,
       filePath: options.filePath,
@@ -2829,7 +2829,10 @@ if (hadRuntime) {
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.socketSeverUrl = void 0;var socketSeverUrl = 'http://192.168.1.102:3002';exports.socketSeverUrl = socketSeverUrl;
+Object.defineProperty(exports, "__esModule", { value: true });exports.BASE_URL = exports.socketSeverUrl = void 0; // export const socketSeverUrl = 'http://localhost:3002'
+// export const BASE_URL = 'http://localhost:3001'
+var socketSeverUrl = 'http://192.168.43.207:3002';exports.socketSeverUrl = socketSeverUrl;
+var BASE_URL = 'http://192.168.43.207:3001';exports.BASE_URL = BASE_URL;
 
 /***/ }),
 
--- util/js/commom.js
+++ util/js/commom.js
@@ -1,1 +1,4 @@
-export const socketSeverUrl = 'http://192.168.1.102:3002'
+// export const socketSeverUrl = 'http://localhost:3002'
+// export const BASE_URL = 'http://localhost:3001'
+export const socketSeverUrl = 'http://192.168.43.207:3002'
+export const BASE_URL = 'http://192.168.43.207:3001'
